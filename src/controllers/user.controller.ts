abdaff6e58d3154560419e410759cbfd101aa134
lehoangvanwangzan <lehoangvan96@gmail.com>
2025-08-03 02:20:49 +0700

    update home page

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,11 +1,12 @@
 import { Request, Response } from "express";
-import { getAllRoles, getAllUsers, getUserById, handleCreateUser, handleDeleteUser, updateUserById } from "services/user.service";
+import { getProducts } from "services/client/item.service";
+import { getAllRoles, getUserById, handleCreateUser, handleDeleteUser, updateUserById } from "services/user.service";
 
 const getHomePage = async (req: Request, res: Response) => {
     //get users
-
-    return res.render("client/home/show.ejs");
-}
+    const products = await getProducts();
+    return res.render("client/home/show.ejs", { products })
+};
 
 const getCreateUserPage = async (req: Request, res: Response) => {
     const roles = await getAllRoles();
